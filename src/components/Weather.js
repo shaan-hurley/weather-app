c70544e8e9cfcb9bb1d3f800e6d62992dc213cec
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-06-07 23:06:39 -0700

    finished app

---
+++ src/components/Weather.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Weather.css'
+
+
+
+const Weather = () => {
+    return (
+        <div className='container'>
+            <h1>Weather App</h1>
+        </div>
+    )
+}
+
+
+
+
+export default Weather;
